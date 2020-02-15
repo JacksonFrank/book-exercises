1b758a3b557d0bc86d1ff8d7bad74d4c81405dc2
JacksonFrank <jacksonrfrank@gmail.com>
2020-02-14 18:47:12 -0800

    Exercises

--- chapter-18-exercises/exercise-1/exercise.R
+++ chapter-18-exercises/exercise-1/exercise.R
@@ -1,16 +1,17 @@
 # load relevant libraries
 library("httr")
 library("jsonlite")
+library("dplyr")
 
 # Be sure and check the README.md for complete instructions!
 
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source("apikey.R")
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "Parasite"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,26 +21,38 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+uri <- paste0(base_uri, resource)
 
+query_params_list <- list("api-key" = nyt_key, "movie_name" = movie_name)
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response <- GET(uri, query = query_params_list)
+response_data <- content(response, "text", encoding = "UTF-8")
+data <- fromJSON(response_data)
 
 # What kind of data structure did this produce? A data frame? A list?
-
+print(is.list(data)) # It return a list
 
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+print(names(data))
+print(str(data)) # the results key in the list points to the content of interest
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- data[["results"]]
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+most_recent_review <- head(reviews, 1) #Reviews already ordered by date from most recent
+recent_headline <- pull(most_recent_review, headline)
+recent_short_summary <- pull(most_recent_review, summary_short)
+recent_link <- pull(most_recent_review, link)[["url"]]
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+recent_review_info <- list(headline = recent_headline, summary = recent_short_summary, link = recent_link)
+print(recent_review_info)
